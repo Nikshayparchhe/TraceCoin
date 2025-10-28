2d2d37a1847d19b08023cd39476600b8a4839e55
Nikshayparchhe <nikshayparchhe@gmail.com>
2025-10-28 13:24:22 +0530

    Update TraceCoin.sol

--- Contarct/TraceCoin.sol
+++ Contarct/TraceCoin.sol
@@ -1,1 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title TraceCoin
+ * @dev A simple token contract for transparent transactions and traceability.
+ */
+contract TraceCoin {
+    string public name = "TraceCoin";
+    string public symbol = "TRC";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    address public owner;
+
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 _initialSupply) {
+        owner = msg.sender;
+        totalSupply = _initialSupply * (10 ** uint256(decimals));
+        balances[owner] = totalSupply;
+        emit Transfer(address(0), owner, totalSupply);
+    }
+
+    /**
+     * @dev Returns the token balance of an account.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    /**
+     * @dev Transfers tokens between addresses.
+     */
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        require(to != address(0), "Invalid recipient");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    /**
+     * @dev Allows tracing of transactions by checking allowance & transferFrom.
+     */
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        require(balances[from] >= amount, "Insufficient balance");
+        require(allowances[from][msg.sender] >= amount, "Allowance exceeded");
+        require(to != address(0), "Invalid recipient");
+
+        balances[from] -= amount;
+        balances[to] += amount;
+        allowances[from][msg.sender] -= amount;
+
+        emit Transfer(from, to, amount);
+        return true;
+    }
+}
 
